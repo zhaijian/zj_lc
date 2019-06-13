5e49ec84b8d94e9b9ea97b4c65d6d39fab4a5b64
zhaijian <zhaijian@51baibao.com>
2019-06-13 11:23:14 +0800

    regular expression matching

---
+++ regular_expression_matching.go
@@ -0,0 +1,61 @@
+package main
+
+//Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+//
+//'.' Matches any single character.
+//'*' Matches zero or more of the preceding element.
+//The matching should cover the entire input string (not partial).
+//
+//Note:
+//
+//s could be empty and contains only lowercase letters a-z.
+//p could be empty and contains only lowercase letters a-z, and characters like . or *.
+//Example 1:
+//
+//Input:
+//s = "aa"
+//p = "a"
+//Output: false
+//Explanation: "a" does not match the entire string "aa".
+//Example 2:
+//
+//Input:
+//s = "aa"
+//p = "a*"
+//Output: true
+//Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+//Example 3:
+//
+//Input:
+//s = "ab"
+//p = ".*"
+//Output: true
+//Explanation: ".*" means "zero or more (*) of any character (.)".
+//Example 4:
+//
+//Input:
+//s = "aab"
+//p = "c*a*b"
+//Output: true
+//Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+//Example 5:
+//
+//Input:
+//s = "mississippi"
+//p = "mis*is*p*."
+//Output: false
+
+//"ab", ".*c"
+func isMatch(s string, p string) bool {
+	if len(p) == 0 {
+		return len(s) == 0
+	}
+
+	firstMatched := len(s) > 0 && (s[0] == p[0] || p[0] == '.')
+
+	if len(p) >= 2 && p[1] == '*' {
+		return isMatch(s, p[2:]) || (firstMatched && isMatch(s[1:], p))
+	} else {
+		return firstMatched && isMatch(s[1:], p[1:])
+	}
+}
